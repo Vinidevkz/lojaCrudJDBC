e977ca1b8dfa617ba036bacf769c8bb121bbd1ad
Vinicius Eduardo <viniynl710@gmail.com>
2026-02-01 17:54:43 -0300

    first commit

---
+++ src/lojaDeGames/DB.java
@@ -0,0 +1,21 @@
+package lojaDeGames;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+public class DB {
+	
+	Connection conn = null;
+	
+	public static Connection connConection() {
+		if(conn == null) {
+			
+		}
+	}
+	
+	public static Properties loadProperties() {
+		try(){
+			
+		}
+	}
+}
