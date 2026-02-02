674524cc16a846c45fc8760aa782dac25cfb0cc2
Vinicius Eduardo <viniynl710@gmail.com>
2026-02-02 13:23:43 -0300

    add DAOs

---
+++ src/model/dao/ClienteDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Cliente;
+
+public interface ClienteDao {
+	void insert(Cliente cliente);
+	void update(Cliente cliente);
+	void deleteByID(Integer id);
+	Cliente findById(Integer id);
+	List<Cliente> findAll();
+}
